8c51086e93821400c1c1967f9c895b1c84b44548
Alin Khatri <alin.khatri@tekkon.com.np>
2023-06-24 16:21:05 +0545

    Initial Commit

---
+++ EsewaClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EsewaClone
+//
+//  Created by Alin Khatri on 24/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
